3ea68947d7af657c1678d44f5d7632e911e59f92
Hasanain Mohammed <hasanain98@gmail.com>
2019-05-30 20:41:32 -0500

    portfoliolooking good

--- assets/javascript/bootstrap.js
+++ assets/javascript/bootstrap.js
@@ -1,18 +1,47 @@
 $(".button-one").on("click", function() {
+    console.log("you clciked me")
   $("html, body").animate({
     scrollTop: $("#about-section").offset().top
   }, 1000);
 })
 
-$(".button-two").on("click", function() {
-  $("html, body").animate({
-    scrollTop: $("#project-section").offset().top
-  }, 1000);
-})
+// project 1 icons
+ScrollReveal().reveal('.p1-1', { delay: 200 });
+ScrollReveal().reveal('.p1-2', { delay: 400 });
+ScrollReveal().reveal('.p1-3', { delay: 600 });
+ScrollReveal().reveal('.p1-4', { delay: 800 });
+ScrollReveal().reveal('.p1-5', { delay: 1000 });
+ScrollReveal().reveal('.p1-6', { delay: 1200 });
+ScrollReveal().reveal('.p1-7', { delay: 1400 });
+ScrollReveal().reveal('.p1-8', { delay: 1600 });
 
-$(".button-three").on("click", function() {
-  $("html, body").animate({
-    scrollTop: $("#contact-section").offset().top
-  }, 1000);
-})
+// project 2 icons
+ScrollReveal().reveal('.p2-1', { delay: 200 });
+ScrollReveal().reveal('.p2-2', { delay: 400 });
+ScrollReveal().reveal('.p2-3', { delay: 600 });
+ScrollReveal().reveal('.p2-4', { delay: 800 });
+ScrollReveal().reveal('.p2-5', { delay: 1000 });
+ScrollReveal().reveal('.p2-6', { delay: 1200 });
+ScrollReveal().reveal('.p2-7', { delay: 1400 });
+ScrollReveal().reveal('.p2-8', { delay: 1600 });
+ScrollReveal().reveal('.p2-9', { delay: 1800 });
 
+// project 3 icons
+ScrollReveal().reveal('.p3-1', { delay: 200 });
+ScrollReveal().reveal('.p3-2', { delay: 400 });
+ScrollReveal().reveal('.p3-3', { delay: 600 });
+ScrollReveal().reveal('.p3-4', { delay: 800 });
+ScrollReveal().reveal('.p3-5', { delay: 1000 });
+ScrollReveal().reveal('.p3-6', { delay: 1200 });
+ScrollReveal().reveal('.p3-7', { delay: 1400 });
+ScrollReveal().reveal('.p3-8', { delay: 1600 });
+ScrollReveal().reveal('.p3-9', { delay: 1800 });
+ScrollReveal().reveal('.p3-10', { delay: 2000 });
+
+// more projects
+ScrollReveal().reveal('.one', { delay: 200 });
+ScrollReveal().reveal('.two', { delay: 400 });
+ScrollReveal().reveal('.three', { delay: 600 });
+ScrollReveal().reveal('.four', { delay: 800 });
+ScrollReveal().reveal('.five', { delay: 1000 });
+ScrollReveal().reveal('.six', { delay: 1200 });
